5febca75461deddc15da94cf5e208462f565a476
TibiaGibon <mordka.private@gmail.com>
2023-06-12 17:25:24 +0200

    update better bg

--- js/bg.js
+++ js/bg.js
@@ -1,46 +1,31 @@
 window.addEventListener("DOMContentLoaded", function () {
   var starsContainer = document.querySelector(".stars");
-  var vh = Math.max(
-    document.documentElement.clientHeight,
-    window.innerHeight || 0
-  );
-  var vw = Math.max(
-    document.documentElement.clientWidth,
-    window.innerWidth || 0
-  );
-  var numStars = (vh + vw) / 10;
-  var resizeTimeout;
+  var numStars = Math.floor((window.innerWidth + window.innerHeight) / 10);
 
-  function generateStars() {
-    starsContainer.innerHTML = "";
-
-    for (var i = 0; i < numStars; i++) {
-      var star = document.createElement("div");
-      star.className = "star";
-      star.style.top = Math.random() * 100 + "%";
-      star.style.left = Math.random() * 100 + "%";
-      star.style.animationDelay = Math.random() + "s";
-      starsContainer.appendChild(star);
-    }
-    // console.log(numStars);
+  for (var i = 0; i < numStars; i++) {
+    var star = document.createElement("div");
+    star.className = "star";
+    star.style.top = Math.random() * 100 + "%";
+    star.style.left = Math.random() * 100 + "%";
+    star.style.animationDelay = Math.random() + "s";
+    starsContainer.appendChild(star);
   }
-  generateStars();
 
+  var resizeTimeout;
   window.addEventListener("resize", function () {
     clearTimeout(resizeTimeout);
-
     resizeTimeout = setTimeout(function () {
-      vh = Math.max(
-        document.documentElement.clientHeight,
-        window.innerHeight || 0
-      );
-      vw = Math.max(
-        document.documentElement.clientWidth,
-        window.innerWidth || 0
-      );
-      numStars = (vh + vw) / 10;
+      starsContainer.innerHTML = "";
+      numStars = Math.floor((window.innerWidth + window.innerHeight) / 10);
 
-      generateStars();
-    }, 10);
+      for (var i = 0; i < numStars; i++) {
+        var star = document.createElement("div");
+        star.className = "star";
+        star.style.top = Math.random() * 100 + "%";
+        star.style.left = Math.random() * 100 + "%";
+        star.style.animationDelay = Math.random() + "s";
+        starsContainer.appendChild(star);
+      }
+    }, 100);
   });
 });
