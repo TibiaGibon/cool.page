8888581b2fdab9c05a8e73f8ecee89fd4308d50c
TibiaGibon <mordka.private@gmail.com>
2023-06-12 20:36:44 +0200

    Try to add mysql

--- js/sr.js
+++ js/sr.js
@@ -1,9 +1,9 @@
 const sr = ScrollReveal({
-  distance: "75px",
+  distance: "50px",
   duration: 3000,
-  delay: 400,
+  delay: 100,
   reset: false,
 });
 
 sr.reveal("#sec1", { delay: 100, origin: "top" });
-sr.reveal("#sec2", { delay: 200, origin: "top" });
+sr.reveal("#sec2", { delay: 100, origin: "top" });
